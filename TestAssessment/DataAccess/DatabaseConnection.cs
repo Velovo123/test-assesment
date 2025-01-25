0af93f55c190c0da1295351264d8fc0f303e8b96
velovo <velovo16@gmail.com>
2025-01-25 11:43:56 +0200

    feat: implement database connection

---
+++ TestAssessment/DataAccess/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAssessment.DataAccess
+{
+    public class DatabaseConnection
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseConnection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
--- TestAssessment/Program.cs
+++ TestAssessment/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("ETL Project Initialized!");
+﻿using Microsoft.Extensions.Configuration;
+using TestAssessment.DataAccess;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var dbConnection = new DatabaseConnection(configuration);
+
+using var connection = dbConnection.CreateConnection();
+
+Console.WriteLine("Database connection initialized successfully.");
